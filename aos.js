f2d05c146f91eae6102524a56d39d96276b34b36
Vitor Buzinaro <funny.hc@gmail.com>
2015-09-23 18:16:40 -0300

    Update aos.js

--- aos.js
+++ aos.js
@@ -1,34 +1,31 @@
-(function() {
-  this.AoS = (function() {
-    function AoS(options) {
-      this.useClass = ".aos";
-      this.offset = 0;
-      
-      if (options) {
-        this.useClass = options.useClass || this.useClass;
-        this.offset = options.offset || this.offset;
+(function(d) {
+  var els;
+  
+  function loaded(cb) {
+    d.readyState === 'complete' ? cb() : d.addEventListener('DOMContentLoaded', cb);
+  }
+  
+  function init() {
+    els = [].slice.call(d.querySelectorAll('[aos]'));
+    hideAll();
+    visibleOnScroll();
+    d.addEventListener('scroll', visibleOnScroll);
+  }
+  
+  function hideAll() {
+    els.forEach(function(el, i) {
+      el.style.visibility = 'hidden';
+    });
+  }
+  
+  function visibleOnScroll() {
+    els.forEach(function(el, i) {
+      if (el.style.visibility === 'hidden' && el.getBoundingClientRect().top - window.innerHeight * 0.8 <= 0) {
+        el.style.visibility = 'visible';
+        el.classList.add('animated', el.getAttribute('aos'));
       }
-    }
-    
-    AoS.prototype.init = function() {
-      var els = document.querySelectorAll(this.useClass);
-      Array.prototype.forEach.call(els, function(el, i){
-        el.style.visibility = "hidden";
-      });
-      document.addEventListener("scroll", this._tryScroll.bind(this));
-    };
-    
-    AoS.prototype._tryScroll = function() {
-      var els = document.querySelectorAll(this.useClass);
-      
-      Array.prototype.forEach.call(els, function(el, i) {
-        if (el.getBoundingClientRect().top - window.innerHeight*0.8 <= this.offset) {
-          el.style.visibility = "visible";
-          el.classList.add("animated", el.getAttribute("data-animation") || "fadeIn");
-        }
-      }.bind(this));
-    };
-    
-    return AoS;
-  })();
-}).call(this);
+    });
+  }
+  
+  loaded(init);
+})(document);
